5a41e3ce2b852a2ef94675be7981307659512f49
Rizwan <rizwandev99@gmail.com>
2024-08-02 19:47:49 +0530

    created first post

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,23 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 const app = new Hono();
 
-app.post("/api/v1/user/signup", (c) => {
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    //@ts-ignore
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
   return c.text("Hello Hono!");
 });
 
