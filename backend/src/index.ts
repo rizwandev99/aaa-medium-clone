dae54fc5b822ff46f2b481c386ba22a0f70504ee
Rizwan <rizwandev99@gmail.com>
2024-08-02 10:36:24 +0530

    Initialised handlers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/user/signup", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/user/signin", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get(" /api/v1/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
