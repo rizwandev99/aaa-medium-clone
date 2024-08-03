3740db2c42b9f562c9b5f1f0adeaa73b4c376927
Rizwan <rizwandev99@gmail.com>
2024-08-03 18:12:25 +0530

    a

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,39 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
+interface CustomContext {
+  jwtPayload?: string;
+  userId?: string;
+}
+interface bindings {
+  DATABASE_URL: "string";
+  JWT_SECRET: "string";
+  // token: "string";
+}
 const app = new Hono<{
-  Bindings: {
-    DATABASE_URL: "string";
-    JWT_SECRET: "string";
-    // token: "string";
-  };
+  Bindings: bindings;
+  Variables: CustomContext;
 }>();
+
+app.use("/api/v1/blog/*", async (c, next) => {
+  const jwt = c.req.header("authorization");
+  if (!jwt) {
+    return c.json({ msg: "No token present" });
+  }
+  const token = await verify(jwt, c.env.JWT_SECRET);
+
+  if (!token) {
+    c.status(401);
+    return c.json({ error: "unAuthorized" });
+  }
+
+  //@ts-ignore
+  c.set("userId", token.id);
+
+  await next();
+});
 
 app.post("/api/v1/user/signup", async (c) => {
   const prisma = new PrismaClient({
